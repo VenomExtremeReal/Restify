36b48ad972921a5696320be58c6b9cf3db43cf6e
VenomExtremeReal <ferreiralucassantos90@gmail.com>
2025-10-24 23:28:14 -0300

    Delete RestifyApp directory

--- RestifyApp/public/test.php
+++
@@ -1,7 +0,0 @@
-<?php
-echo "PHP está funcionando!<br>";
-echo "BASE_URL seria: " . (isset($_SERVER['HTTP_HOST']) ? 'http://' . $_SERVER['HTTP_HOST'] . dirname($_SERVER['SCRIPT_NAME']) : 'não definido') . "<br>";
-echo "REQUEST_URI: " . $_SERVER['REQUEST_URI'] . "<br>";
-echo "SCRIPT_NAME: " . $_SERVER['SCRIPT_NAME'] . "<br>";
-phpinfo();
-?>
