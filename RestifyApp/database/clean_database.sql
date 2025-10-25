730809a6102ca1f80fab53fc38fd07903ae82d82
VenomExtremeReal <ferreiralucassantos90@gmail.com>
2025-10-25 11:07:42 -0300

    Delete RestifyApp directory

--- RestifyApp/database/clean_database.sql
+++
@@ -1,7 +0,0 @@
--- Script para limpar dados do banco SQLite
-DELETE FROM messages;
-DELETE FROM order_items;
-DELETE FROM orders;
-DELETE FROM restaurants WHERE email != 'admin@restify.com';
-DELETE FROM sqlite_sequence WHERE name IN ('messages', 'order_items', 'orders', 'restaurants');
-INSERT INTO sqlite_sequence (name, seq) VALUES ('restaurants', 1);
